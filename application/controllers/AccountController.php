fa40795b840b641d5cb5c9888304653d39fc6123
andrehigher <andrelopesgonzaga@gmail.com>
2013-06-05 00:17:03 -0300

    Edição de dados

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -5,7 +5,10 @@ class AccountController extends Zend_Controller_Action
 
     public function init()
     {
-        $this->_helper->layout()->setLayout('dashboard');
+      $this->_helper->layout()->setLayout('dashboard');
+      $user = new Application_Model_DbTable_User();
+			$authNamespace = new Zend_Session_Namespace('userInformation');
+      $this->view->user = $user->fetchRow($user->select()->where('id = ?',$authNamespace->user_id));
     }
 
     public function indexAction()
@@ -25,12 +28,44 @@ class AccountController extends Zend_Controller_Action
 
     public function personalAction()
     {
-        // action body
+      if ( $this->getRequest()->isPost() ) 
+      {
+      	try{
+	        $data = $this->getRequest()->getPost();
+	        $user = new Application_Model_DbTable_User();
+					$authNamespace = new Zend_Session_Namespace('userInformation');
+	      	$userRow = $user->fetchRow($user->select()->where('id = ?',$authNamespace->user_id));
+	      	$userRow->name = $data['name'];
+	      	$userRow->phone = $data['phone'];
+	      	$userRow->email = $data['email'];
+	      	$userRow->save();
+	      	$this->view->user = $userRow;
+	      	$this->view->success = true;
+	      }catch(Zend_Exception $e){
+	      	$this->view->error = true;
+	      }
+      }
     }
 
     public function passwordAction()
     {
-        // action body
+      if ( $this->getRequest()->isPost() ) 
+      {
+      	$data = $this->getRequest()->getPost();
+        $user = new Application_Model_DbTable_User();
+				$authNamespace = new Zend_Session_Namespace('userInformation');
+      	$userRow = $user->fetchRow($user->select()->where('id = ?',$authNamespace->user_id));
+      	if($userRow->password == sha1($data['old_password']) && $data['confirm_password'] == $data['password'])
+      	{
+      		$userRow->password = sha1($data['password']);
+      		$userRow->save();
+	      	$this->view->success = true;
+      	}
+      	else
+      	{
+      		$this->view->password_error = true;
+      	}
+      }
     }
 
 
