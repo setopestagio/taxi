1ef75a7740f2a4b6dc2fd8a620a3715de7a1917e
andrehigher <andrelopesgonzaga@gmail.com>
2013-07-02 11:57:17 -0300

    Auxiliar card

--- public/js/general.js
+++ public/js/general.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$(".phone").mask("(99) 9999-9999");
 	$(".cpf").mask("999.999.999-99");  
-	$(".permission").mask("aa 9999"); 
+	$(".permission").mask("aa 9999");
+	$(".zipcode").mask("99999-999");  
 	$('.help').tooltip();
   $('.datepicker').datepicker({
     language: 'pt-BR',
