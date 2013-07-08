895742997f19f3f2c643ec5695416b5b2c58e950
andrehigher <andrelopesgonzaga@gmail.com>
2013-07-08 10:29:53 -0300

    Vis

--- public/js/vis.js
+++ public/js/vis.js
@@ -1,60 +1,92 @@
 
 $(document).ready(function(){
 
-  var data = [];
-  $.ajax({
-    url: '/js/events.json',
-    async: false,
-    dataType: 'json',
-    success: function (json) {
-      data = json;
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  var formatPercent = d3.format("");
+
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1, 1);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select("#visTaxis").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.tsv("/js/data.tsv", function(error, data) {
+
+    data.forEach(function(d) {
+      d.frequency = +d.frequency;
+    });
+
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Quantidade");
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.frequency); })
+        .attr("height", function(d) { return height - y(d.frequency); });
+
+    d3.select("input").on("change", change);
+
+    var sortTimeout = setTimeout(function() {
+      d3.select("input").property("checked", true).each(change);
+    }, 2000);
+
+    function change() {
+      clearTimeout(sortTimeout);
+
+      // Copy-on-write since tweens are evaluated after a delay.
+      var x0 = x.domain(data.sort(this.checked
+          ? function(a, b) { return b.frequency - a.frequency; }
+          : function(a, b) { return d3.ascending(a.letter, b.letter); })
+          .map(function(d) { return d.letter; }))
+          .copy();
+
+      var transition = svg.transition().duration(750),
+          delay = function(d, i) { return i * 50; };
+
+      transition.selectAll(".bar")
+          .delay(delay)
+          .attr("x", function(d) { return x0(d.letter); });
+
+      transition.select(".x.axis")
+          .call(xAxis)
+        .selectAll("g")
+          .delay(delay);
     }
   });
-
-  var barWidth = 40;
-  var width = (barWidth + 10) * data.length;
-  var height = 300;
-
-  var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
-  var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.total; })])
-                            .rangeRound([0, height]);
-
-  var svg = d3.select("#visTaxis")
-    .append("svg:svg")
-    .attr("width", width)
-    .attr("height", height);
-
-  svg.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('svg:rect')
-    .attr("x", function(datum, index) { return x(index); })
-    .attr("y", function(datum) { return height - y(datum.total); })
-    .attr("height", function(datum) { return y(datum.total); })
-    .attr("width", barWidth)
-    .attr("fill", "#2d578b");
-
-  svg.selectAll("text")
-    .data(data)
-    .enter()
-    .append("svg:text")
-    .attr("x", function(datum, index) { return x(index) + barWidth; })
-    .attr("y", function(datum) { return height - y(datum.total); })
-    .attr("dx", -barWidth/2)
-    .attr("dy", "1.2em")
-    .attr("text-anchor", "middle")
-    .text(function(datum) { return datum.total;})
-    .attr("fill", "white");
-
-  svg.selectAll("text.yAxis")
-    .data(data)
-    .enter().append("svg:text")
-    .attr("x", function(datum, index) { return x(index) + barWidth; })
-    .attr("y", height)
-    .attr("dx", -barWidth/2)
-    .attr("text-anchor", "middle")
-    .attr("style", "font-size: 12; font-family: Helvetica, sans-serif;")
-    .text(function(datum) { return datum.car;})
-    .attr("transform", "translate(0, 18)")
-    .attr("class", "yAxis");
 });
