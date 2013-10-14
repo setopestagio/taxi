b6059965bedb5e0112e71120e377bee5709aafb7
andrehigher <andrelopesgonzaga@gmail.com>
2013-10-14 14:21:48 -0300

    Save new grantee without vehicle

--- application/controllers/GranteeController.php
+++ application/controllers/GranteeController.php
@@ -36,7 +36,6 @@ class GranteeController extends Zend_Controller_Action
         }
       }catch(Zend_Exception $e){
         $this->view->error = true;
-        echo $e->getMessage();
       }
       $vehicleModel = new Application_Model_DbTable_VehicleModel();
       $this->view->vehicleModel = $vehicleModel->fetchAll($vehicleModel->select()->order('name'));
